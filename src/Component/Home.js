5819a6eb17dcf9936daf09a0b27c2fe4b0a3546b
shilpamewada <156938495+shilpamewada@users.noreply.github.com>
2025-12-10 10:59:46 +0000

    add image

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -2,12 +2,13 @@
 import Products from './Products';
 import Contact from './Contact';
 import Images from './Images';
+import bgimage from './bg.jpg';
 
 function Home({ addToCart }) {
   return (
     <div className='hero'>
       <div className="card text-bg-dark text-white border-0">
-      <img src="/assets/bg.jpg" className="card-img" alt="Background" height="550px" />
+      <img src={bgimage} className="card-img" alt="Background" height="550px" />
         <div className="card-img-overlay d-flex flex-column justify-content-center ">
           <div className="container">
             <h5 className="card-title  display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
